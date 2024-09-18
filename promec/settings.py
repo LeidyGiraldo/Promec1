c63522fc7d8f887f1ce30e53d49c059e771bc17b
Leidy <leivvalbuena1@soy.sena.edu.co>
2024-09-18 17:08:48 -0500

    Update Settings

--- promec/settings.py
+++ promec/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-98slg(i+&+&r-ygjtwo&8=oek+lm__*6j2$r2gnoj6@)7w(_mc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
